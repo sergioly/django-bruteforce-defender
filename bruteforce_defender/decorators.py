cc4be22bdcd233c5f74a75042b3823c89b8438ea
Sergey Lysach <sergey.lysach@gmail.com>
2013-08-18 10:21:17 +0300

    Fixed settings import

--- bruteforce_defender/decorators.py
+++ bruteforce_defender/decorators.py
@@ -5,19 +5,13 @@ Decorator for bruteforce_defender application.
 from __future__ import unicode_literals
 import redis
 
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect
 
+from bruteforce_defender.settings import (REDIS_HOST, REDIS_PORT,
+    REDIS_PASSWORD, REDIS_DB, LOGIN_ATTEMPTS, BLOCKING_TIME)
 
-REDIS_HOST = getattr(settings, 'REDIS_HOST', 'localhost')
-REDIS_PORT = getattr(settings, 'REDIS_PORT', 6379)
-REDIS_PASSWORD = getattr(settings, 'REDIS_PASSWORD', '')
-REDIS_DB = getattr(settings, 'DF_REDIS_DB', 0)
-
-LOGIN_ATTEMPTS = getattr(settings, 'LOGIN_ATTEMPTS', 10)
-BLOCKING_TIME = getattr(settings, 'BLOCKING_TIME', 10*60) # 10min = 600sec
 
 message = "You exceeded the maximum allowed number of login attempts. Try again later"
 
